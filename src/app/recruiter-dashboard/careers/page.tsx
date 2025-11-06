9e15f6b1ea3176978889693caadcd3a17202fb5f
filsammy <filsammy@gmail.com>
2025-11-07 07:23:02 +0800

    fixed fetch typo (#1)
    fixed fetch typo and ensured a working branch

--- src/app/recruiter-dashboard/careers/page.tsx
+++ src/app/recruiter-dashboard/careers/page.tsx
@@ -7,7 +7,11 @@ import HeaderBar from "@/lib/PageComponent/HeaderBar";
 export default function () {
   return (
     <>
-      <HeaderBar activeLink="Careers" currentPage="Overview" icon="la la-suitcase" />
+      <HeaderBar
+        activeLink="Careers"
+        currentPage="Overview"
+        icon="la la-suitcase"
+      />
       <div className="container-fluid mt--7" style={{ paddingTop: "6rem" }}>
         <div className="row">
           <div className="col">
